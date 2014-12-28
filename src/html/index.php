d4208e52c15b4ec4053ab635c55206966527b25b
Max840 <maxime.pl@outlook.com>
2014-12-28 08:10:12 +0000

    Removes from the list every programming language with less than 25 repositories using it as their main language

--- src/html/index.php
+++ src/html/index.php
@@ -8,8 +8,11 @@ error_reporting(E_ALL | E_STRICT);
 
 $repoCache = new RepoCache($config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['dbName']);
 
-// For the language filter
-$langList = $repoCache->langList();
+/* For the language filter.
+ * Removes every programming language with less than 25 repositories using it as their main language
+ * to prevent random.php from always returning the same repositories.
+ */
+$langList = $repoCache->langList(25);
 ?>
 
 <!DOCTYPE html>
