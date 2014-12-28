24726a0724f556dc74f5f29e4e6f1430d0580609
Max840 <maxime.pl@outlook.com>
2014-12-28 07:18:57 +0000

    Supports the language filter

--- src/html/random.php
+++ src/html/random.php
@@ -7,10 +7,16 @@ $config = include(dirname(__FILE__) . '/../config.php');
 ini_set('display_errors', 'Off');
 error_reporting(E_ALL | E_STRICT);
 
+$language = null;
+// Checking if a language filter is specified
+if (isset($_GET['lang'])) {
+    $language = $_GET['lang'];
+}
+
 try {
     $repoCache = new RepoCache($config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['dbName']);
     
-    $randomRepo = $repoCache->randomRepo();
+    $randomRepo = $repoCache->randomRepo($language);
     header('location: ' . $randomRepo->getUrl());
 } catch (Exception $e) {
     echo 'Something wrong happened';
