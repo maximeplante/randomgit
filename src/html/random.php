7b37eae5dd77f01bb84fdc12ce1c70ea013aa4b6
Max840 <maxime.pl@outlook.com>
2015-01-08 16:19:15 +0000

    Put back random.php

---
+++ src/html/random.php
@@ -0,0 +1,29 @@
+<?php
+include(dirname(__FILE__) . '/../Classes/Repo.php');
+include(dirname(__FILE__) . '/../Classes/RepoCache.php');
+$config = include(dirname(__FILE__) . '/../config.php');
+
+if ($config['debug']){
+    ini_set('display_errors', 'On');
+} else {
+    ini_set('display_errors', 'Off');
+}
+error_reporting(E_ALL | E_STRICT);
+
+$language = null;
+// Checking if a language filter is specified
+if (isset($_GET['lang'])) {
+    $language = $_GET['lang'];
+}
+
+try {
+    $repoCache = new RepoCache($config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['dbName']);
+    
+    $randomRepo = $repoCache->randomRepo($language);
+    
+    header('location: ' . $randomRepo->getUrl());
+    
+} catch (Exception $e) {
+    http_response_code(500);
+    throw $e;
+}
