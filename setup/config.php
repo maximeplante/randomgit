8b435d858e6faab1d1d7475684fd5121e04720a3
Maxence Frenette <maxence.frenette@gmail.com>
2015-01-09 00:13:40 -0500

    Clarified comment

--- setup/config.php
+++ setup/config.php
@@ -8,11 +8,18 @@ return array(
         'pass' => '',
         'dbName' => ''
     ),
+    
     // Tells if the website is in debug mode
     'debug' => true,
-    /* You need to register an application at
+    
+    /*
+     * You need to register an application at
      * https://github.com/settings/applications/new
      * to get an OAuth client id and secret.
+     * 
+     * For developpement purposes, this can be left empty.
+     * It will instead use the public github API. The only
+     * donwside is that less repos will be fetched at a time.
      */
     'githubAPI' => array(
         'OAuth' => array(
