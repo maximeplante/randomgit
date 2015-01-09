4dd96e4850fc91dbad25b6f2122004a3f71191de
Max840 <maxime.pl@outlook.com>
2015-01-09 06:27:20 +0000

    Now the database is locked only after the script have finished his transactions with the GitHub API

--- src/Scripts/updateRepo.php
+++ src/Scripts/updateRepo.php
@@ -18,29 +18,37 @@ $github = new GitHub($config['githubAPI']['OAuth']['id'], $config['githubAPI']['
 
 $repoCache = new RepoCache($config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['dbName']);
 
+try {
+    // Fetching new GitHub repositories until it reaches the rate limit of the GitHub API
+    $randomRepoList = array();
+    // Does multiple fetches to get more repositories
+    for ($i = 0;  $i < 5; $i++) {
+        $randomRepoList = array_merge($randomRepoList, $github->getRandomRepoList(true));
+    }
+} catch (GitHubAPIRateLimitException $e) {
+    /* Handles gracefully the case when the script goes over the rate limit of
+     * the GitHub API. Nothing to do here since it is expected.
+     */
+}
+
 // Locks the database to prevent random.php from accessing the repository cache while the ranks are not assigned yet
 $repoCache->lock();
 
-try {
-    // Fetching new GitHub repositories until it reaches the rate limit of the GitHub API
-    while (true) {
-        $randomRepoList = $github->getRandomRepoList(true);
-        
-        foreach ($randomRepoList as $repo) {
-            // Do not add duplicates
-            if (!$repoCache->isCached($repo)) {
-                $repoCache->storeRepo($repo);
-            }
-        }
+// Stores the repositories in the cache
+foreach ($randomRepoList as $repo) {
+    // Do not add duplicates
+    if (!$repoCache->isCached($repo)) {
+        $repoCache->storeRepo($repo);
     }
-} catch (GitHubAPIRateLimitException $e) {
-    // Randomly removes repositories from the cache to keep their count under RepoCache::MAX_REPOCACHE_SIZE
-    $repoOverflowCount = $repoCache->count() - RepoCache::MAX_REPOCACHE_SIZE;
-    if ($repoOverflowCount > 0) {
-        // Deletes more than just the overflow to prevent repositories from staying for ever in the cache
-        $repoCache->randomRemove($repoOverflowCount + ceil(RepoCache::MAX_REPOCACHE_SIZE / 4));
-    }
-    // Assigns a rank to every repository (used in RepoCache::randomRepo())
-    $repoCache->giveRanks();
-    exit('Success');
 }
+
+// Randomly removes repositories from the cache to keep their count under RepoCache::MAX_REPOCACHE_SIZE
+$repoOverflowCount = $repoCache->count() - RepoCache::MAX_REPOCACHE_SIZE;
+if ($repoOverflowCount > 0) {
+    // Deletes more than just the overflow to prevent repositories from staying for ever in the cache
+    $repoCache->randomRemove($repoOverflowCount + ceil(RepoCache::MAX_REPOCACHE_SIZE / 4));
+}
+// Assigns a rank to every repository (used in RepoCache::randomRepo())
+$repoCache->giveRanks();
+
+exit('Success');
