add139dcc5a6cf588c75823935123ca02d5b2989
Maxence Frenette <maxence.frenette@gmail.com>
2015-01-11 20:34:12 -0500

    Added jQuery

--- src/html/index.php
+++ src/html/index.php
@@ -98,6 +98,7 @@ $langList = $repoCache->langList(25);
             </div>
         </div>
         
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="js/libs/bootstrap.min.js"></script>
         <script src="js/script.js"></script>
         
