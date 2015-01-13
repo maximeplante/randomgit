97a932cbe58bcffc34b433591b994795bc594dde
Max840 <maxime.pl@outlook.com>
2015-01-13 02:45:16 +0000

    The client can ask for mutiple repositories in one request by specifying the 'limit' url argument

--- src/html/ajax/random.php
+++ src/html/ajax/random.php
@@ -16,26 +16,44 @@ if (isset($_GET['lang'])) {
     $language = $_GET['lang'];
 }
 
+$limit = 1;
+// Checking if the client wants more than one repository (max 10)
+if (isset($_GET['limit']) && intval($_GET['limit']) > 0) {
+    if (intval($_GET['limit']) > 10) {
+        $limit = 10;
+    } else {
+        $limit = intval($_GET['limit']);
+    }
+}
+
 try {
     $repoCache = new RepoCache($config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['dbName']);
     
-    $randomRepo = $repoCache->randomRepo($language);
+    $repoList = array();
     
-    $repoData = array(
-        array(
+    // Fills the array with the data of the repositories
+    for ($i = 0; $i < $limit; $i++) {
+        
+        $randomRepo = $repoCache->randomRepo($language);
+        
+        $repo = array(
             'name' => $randomRepo->getName(),
             'user' => $randomRepo->getUser(),
             'url' => $randomRepo->getUrl(),
             'lang' => $randomRepo->getLang(),
             'readme_html' => $randomRepo->getReadmeHTML()
-        )
-    );
+        );
+        
+        array_push($repoList, $repo);
+        
+    }
     
     http_response_code(200);
     
     header('Content-Type: application/json');
     
-    echo json_encode($repoData);
+    // Sends the json data to the client
+    echo json_encode($repoList);
     
 } catch (Exception $e) {
     http_response_code(500);
