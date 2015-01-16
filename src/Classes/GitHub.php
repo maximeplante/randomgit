42fcb11cb00018dbd0cdc33ca4daf1a411028a98
Max840 <maxime.pl@outlook.com>
2015-01-16 01:55:41 +0000

    Created GitHub::request() to reduce code repetition

--- src/Classes/GitHub.php
+++ src/Classes/GitHub.php
@@ -52,22 +52,11 @@ class GitHub
     // Returns an array of repositories matching the search query
     public function searchRepo($query)
     {
-        $headers = array('User-Agent' => $this->userAgent);
-        
-        try {
-            $url = 'https://api.github.com/search/repositories?q=' . urlencode($query)
-                . '&client_id=' . urlencode($this->oAuthId)
-                . '&client_secret=' . urlencode($this->oAuthSecret);
-            $response = Requests::get($url, $headers);
-        } catch (Requests_Exception $e) {
-            throw new ConnectionException('Unable to reach the GitHub API', 0);
-        }
-        
-        if ($response->status_code === 403) {
-            throw new GitHubAPIRateLimitException('Rate limit fo the GitHub API is exceeded', 0);
-        } else if(!$response->success) {
-            throw new GitHubAPIException('The GitHub API encountered an error. Raw response body : ' . $response->body, 0);
-        }
+        $response = $this->request('https://api.github.com/search/repositories',
+         array('q' => $query),
+         array(),
+         true
+        );
         
         $rawRepoList = json_decode($response->body);
         
@@ -89,30 +78,64 @@ class GitHub
     // Returns the readme (html) of a repository
     private function getReadmeHTML($repoName, $repoUser)
     {
-        $headers = array(
-            'User-Agent' => $this->userAgent,
-            'Accept' => 'application/vnd.github.v3.html'
-        );
         
         try {
-            $url = 'https://api.github.com/repos/' . urlencode($repoUser) . '/'. urlencode($repoName) . '/readme'
-                . '?client_id=' . urlencode($this->oAuthId)
-                . '&client_secret=' . urlencode($this->oAuthSecret);
-            $response = Requests::get($url, $headers);
+            $response = $this->request('https://api.github.com/repos/' . urlencode($repoUser) . '/'. urlencode($repoName) . '/readme',
+             array(),
+             array('Accept' => 'application/vnd.github.v3.html'),
+             true
+            );
+        } catch (GitHubAPINotFoundException $e) {
+            throw new GitHubAPINotFoundException('Cannot find the readme associated with a respository.', 0);
+        }
+        
+        return $response->body;
+    }
+    
+    /* Does a request to the GitHub API
+     * It always add the User-Agent to the headers since it is required by the API
+     * string $url : the url of the request
+     * array $parameters : the url parameters (no need to use urlencode)
+     * array $headers : the HTTP headers of the request
+     * bool $userAgent : add the User-Agent to the headers
+     * bool $oAuth : add OAuth data to the url
+     * Returns the response in a Requests_Response object
+     */
+    private function request($url, $parameters, $headers, $oAuth)
+    {
+        try {
+            $fullUrl = $url . '?';
+            
+            // Appending the url parameters
+            foreach ($parameters as $key => $value) {
+                $fullUrl .= urlencode($key) . '=' . urlencode($value) . '&';
+            }
+            
+            if ($oAuth) {
+                // Appending the OAuth url parameters
+                $fullUrl .= 'client_id=' . urlencode($this->oAuthId) .
+                            '&client_secret=' . urlencode($this->oAuthSecret);
+            }
+            
+            // Adding the User-Agent to the headers
+            $headers['User-Agent'] = $this->userAgent;
+            
+            $response = Requests::get($fullUrl, $headers);
+        
         } catch (Requests_Exception $e) {
             throw new ConnectionException('Unable to reach the GitHub API', 0);
         }
         
         if ($response->status_code === 404) {
-            throw new GitHubAPINotFoundException('Cannot find the readme associated with a respository.', 0);
+            throw new GitHubAPINotFoundException('Cannot find the requested data.', 0);
         }
         
         if ($response->status_code === 403) {
-            throw new GitHubAPIRateLimitException('Rate limit fo the GitHub API is exceeded', 0);
+            throw new GitHubAPIRateLimitException('Rate limit of the GitHub API is exceeded', 0);
         } else if(!$response->success) {
             throw new GitHubAPIException('The GitHub API encountered an error. Raw response body : ' . $response->body, 0);
         }
         
-        return $response->body;
+        return $response;
     }
 }
